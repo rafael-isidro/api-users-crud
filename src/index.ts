5f487686a75a6ffe1f0950fbf7be1c41a504ded2
rafael-isidro <rafa-isidro@hotmail.com>
2023-11-05 18:59:36 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+import express from "express";
+import { config } from "dotenv";
+config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+ 
