9a0a757ceba2650ecfa22bc2b97ab03fcd2a0e7f
rafael-isidro <rafa-isidro@hotmail.com>
2023-11-06 01:20:08 -0300

    feat: add CreateUserController with validations

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -1,4 +1,10 @@
 export interface HttpResponse<T> {
-    statusCode: number
-    body: T | string;
+  statusCode: number;
+  body: T | string;
+}
+
+export interface HttpRequest<B> {
+  params?: any;
+  headers?: any;
+  body?: B;
 }
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import express from "express";
 import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
 import { MongoClient } from "./database/mongo";
+import { CreateUserController } from "./controllers/create-user/create-user";
 
 const main = async () => {
   config();
-  
+
   const app = express();
+  app.use(express.json());
 
   const PORT = process.env.PORT || 8000;
 
@@ -19,7 +22,20 @@ const main = async () => {
 
     const { body, statusCode } = await getUsersController.handle();
 
-    res.send(body).status(statusCode);
+    res.status(statusCode).send(body);
+  });
+
+  app.post("/users", async (req, res) => {
+    const mongoCreateUserRepository = new MongoCreateUserRepository();
+    const createUserController = new CreateUserController(
+      mongoCreateUserRepository
+    );
+
+    const { body, statusCode } = await createUserController.handle({
+      body: req.body,
+    });
+
+    res.status(statusCode).send(body);
   });
 
   app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
